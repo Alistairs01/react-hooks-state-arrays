8a103d69750db568be00afaad938226e915c5ef1
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-04-26 15:04:09 +0300

    multiple state variables

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,75 +3,13 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
 
-  function handleAddFood() {
-     // create new array with all the elements
-    // of the original array
-    
-    /**
-     * Here we are using the spread operator to make a copy of our foods array
-      and insert each element into the new array
-      We are also adding the newly generated food returned by the 
-      getNewRandomSpicyFood function at the end of the array
-     */
-    /**
-     * Remember whenever we are updating state
-     * it is important that we always pass a new array/object to the setState
-     * thats why we are using the spread operator here to make a copy of the array
-     * instead of .push which will mutate the original array/object
-     */
-    /**
-     * After setting state , our component should automatically 
-     * re-render with the new list of foods 
-     */
-    const newFood = getNewRandomSpicyFood();
-    const newFoodArray = [...foods, newFood];
-    setFoods(newFoodArray);
-  }
+  const foodsToDisplay = foods.filter((food) => {
+    return filterBy === "All" || food.cuisine === filterBy;
+  });
 
-  function handleDeleteFood(id) {
-     /**
-     * Calling .filter will return a new array based on which elements 
-     * match our criteria in the call back function 
-     * So if we write our callback function in .filter to look for all the 
-     * foods except the number were trying to remove 
-     * well get back a new shortened list of foods 
-     */
-    /**
-     * Setting state with this updated list of foods will 
-     * re-render our component , causing the food to be removed from the list.
-     */
-
-
-    const newFoodArray = foods.filter((food) => food.id !== id);
-    setFoods(newFoodArray);
-  }
-
-  /**
-   To break down the steps:
-
-.map will iterate through the array and return a new array
-Whatever value is returned by the callback function that we pass to .
-map will be added to this new array
-If the ID of the food we're iterating over matches the ID of the food we're updating
-, return a new food object with the heat level incremented by 1
-Otherwise, return the original food object
-   */
-
-  function handleAddHeat(id) {
-    const updatedFoods = foods.map((food) => {
-      if (food.id === id) {
-        return   { ...food, heatLevel: food.heatLevel + 1, 
-        };
-       } else {
-          return food;
-        }
-      }
-    );
-    setFoods(updatedFoods);
-  }
-
-  const foodList = foods.map((food) => (
+  const foodList = foodsToDisplay.map((food) => (
     <li key={food.id}>
       <FoodCard
         food={food}
@@ -81,9 +19,41 @@ Otherwise, return the original food object
     </li>
   ));
 
+  function handleAddFood() {
+    const newFood = getNewRandomSpicyFood();
+    setFoods([...foods, newFood]);
+  }
+
+  function handleDeleteFood(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+
+  function handleAddHeat(id) {
+    const updatedFoods = foods.map((food) => {
+      if (food.id === id) {
+        return { ...food, heatLevel: food.heatLevel + 1 };
+      } else {
+        return food;
+      }
+    });
+    setFoods(updatedFoods);
+  }
+
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
+      <select name="filter" value={filterBy} onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <ul>{foodList}</ul>
     </div>
   );
